508d029239661d78dd70fef24d2db6ca5783c26e
coufal2 <134766618+coufal2@users.noreply.github.com>
2024-05-15 12:57:16 +0200

    Update table.php
       <script type="text/javascript" charset="utf8" src="vendor/jquery.dataTables.js"></script>

--- table.php
+++ table.php
@@ -31,7 +31,7 @@ include "helper.php";
     
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
+    <script type="text/javascript" charset="utf8" src="vendor/jquery.dataTables.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.js"></script>
  
     
